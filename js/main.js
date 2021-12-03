9ac3b286d95eac8f9ee7f698f84977933f07c35b
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2021-12-02 23:15:41 -0500

    undid previous commit and commented out main,js

--- js/main.js
+++ js/main.js
@@ -1,37 +1,37 @@
-import { weatherAPI, quotesAPI } from './api';
-import dotenv from 'dotenv';
+// import { weatherAPI, quotesAPI } from './api';
+// import dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
 
-let zipCodeButton = document.getElementById("zip-code-button");
+// let zipCodeButton = document.getElementById("zip-code-button");
 
-zipCodeButton.addEventListener("click", () => {
-  let zipCode = document.getElementById("zip-code").value;
+// zipCodeButton.addEventListener("click", () => {
+//   let zipCode = document.getElementById("zip-code").value;
 
-  weatherAPI(zipCode)
-    .then(res => {
-      // console.log(res);
-      setWeather(res.name, res.weather[0].description);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-});
+//   weatherAPI(zipCode)
+//     .then(res => {
+//       // console.log(res);
+//       setWeather(res.name, res.weather[0].description);
+//     })
+//     .catch(err => {
+//       console.log(err);
+//     });
+// });
 
-quotesAPI()
-  .then(res => {
-    // console.log(res);
-    setQuote(res.content, res.author);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+// quotesAPI()
+//   .then(res => {
+//     // console.log(res);
+//     setQuote(res.content, res.author);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
 
-let setWeather = (name, weather) => {
-  document.getElementById("weather-name").innerHTML = name;
-  document.getElementById("weather").innerHTML = weather;
-}
-let setQuote = (quote, author) => {
-  document.getElementById("quote").innerHTML = quote;
-  document.getElementById("quote-author").innerHTML = author;
-}
+// let setWeather = (name, weather) => {
+//   document.getElementById("weather-name").innerHTML = name;
+//   document.getElementById("weather").innerHTML = weather;
+// }
+// let setQuote = (quote, author) => {
+//   document.getElementById("quote").innerHTML = quote;
+//   document.getElementById("quote-author").innerHTML = author;
+// }
