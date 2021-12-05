f7b91b835b0ee5022524e3e1f23b70cf4314983c
Miguel <miguel.castillo-melendez81@bcmail.cuny.edu>
2021-12-04 20:12:55 -0500

    finished temperature widget.

--- js/api.js
+++ js/api.js
@@ -1,8 +1,16 @@
 import axios from "axios";
 
 // WeatherAPI
-const weatherAPI = (zipCode) => {
-  let url = axios.get(`${process.env.OW_BASE}${zipCode}&appid=${process.env.OW_API_KEY}`);
+// const weatherAPI = (zipCode) => {
+//   let url = axios.get(`${process.env.OW_BASE}${zipCode}&appid=${process.env.OW_API_KEY}`);
+
+//   const res = url.then(response => { return response.data; }).catch(err => { console.error(err); });
+
+//   return res;
+// };
+
+const weatherAPI = () => {
+  let url = axios.get(`${process.env.OW_BASE}11218&appid=${process.env.OW_API_KEY}`);
 
   const res = url.then(response => { return response.data; }).catch(err => { console.error(err); });
 
--- js/main.js
+++ js/main.js
@@ -23,6 +23,34 @@ import { weatherAPI, quotesAPI } from './api';
 //   document.getElementById("weather").innerHTML = weather;
 // }
 
+weatherAPI()
+  .then(res => {
+    console.log(res);
+    // Returns a kelvin temperature so we need to convert it to fahrenheit and round it to nearest whole number (Math.round) and then convert it to a string (toFixed(0)) and then add the '°' symbol to the end of the string.
+    setWeather(res.name, res.weather[0].icon, res.main.temp, res.main.temp_max, res.main.temp_min);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+let setWeather = (name, weatherIcon, temp, hi, lo) => {
+  document.getElementById("location").innerHTML = name;
+  document.getElementById("weather-icon").src = weatherIcon;
+  let src = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
+  document.getElementById("weather-icon").setAttribute("src", src);
+
+  // Convert Kelvin to fahrenheit and round it to nearest whole number (Math.round) and then convert it to a string (toFixed(0)) and then add the '°' symbol to the end of the string.
+  temp = Math.round((temp * 9) / 5 - 459.67).toFixed(0);
+
+  document.getElementById("temperature").innerHTML = `${temp}°F`;
+
+  hi = Math.round((hi * 9) / 5 - 459.67).toFixed(0);
+  lo = Math.round((lo * 9) / 5 - 459.67).toFixed(0);
+
+  document.getElementById("hi").innerHTML = `High: ${hi}°F`;
+  document.getElementById("lo").innerHTML = `Low: ${lo}°F`;
+};
+
 quotesAPI()
   .then(res => {
     // console.log(res);
